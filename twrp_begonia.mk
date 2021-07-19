d895bd7ed30f8ee380ce3881d794e8e553b8304b
TheMalachite <eliasgheeraert@gmail.com>
2021-07-20 00:42:40 +0200

    begonia: Set UTC date to 0
    Change-Id: I921c74f2d74cd16d4974ff1ae8f0860f43a64e41

--- twrp_begonia.mk
+++ twrp_begonia.mk
@@ -34,4 +34,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
+    ro.vendor.build.security_patch=2099-12-31 \
+    ro.bootimage.build.date.utc=0 \
+    ro.build.date.utc=0
